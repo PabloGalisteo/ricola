40dada7e7cf4cac0c873db43ac6288526d48e96d
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-03-03 16:25:31 +0100

    Made Popup work through JavaScript and added cross with Font Awesome

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const climberBtn = document.querySelector(".climber-btn")
+const popUp = document.querySelector(".popup")
+
 
 climberBtn.addEventListener("click", () => {
-    alert('Hej!')
+    hiddenPopup();
 });
+
+function hiddenPopup() {
+    popUp.classList.remove("hidden");
+}
 
 
 
