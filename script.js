c93718c5c3fcb7602c216b689e98757b0352a6fa
Mieown <malinolofsson@hyperisland.se>
2021-03-03 12:16:33 +0100

    added a popup image and started little bit with HS

---
+++ script.js
@@ -0,0 +1,6 @@
+const climberBtn = document.querySelector(".climber-btn");
+
+
+function setupEventListeners {
+    document.getElementById("climber-btn").addEventListener("click", alert("Hejsan");
+}  
