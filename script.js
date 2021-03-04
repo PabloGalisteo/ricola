81713e89aa49c95979b329f67b020b4d1d4fbfad
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-03-04 16:49:16 +0100

    styled popup with right fonts and paddings

--- script.js
+++ script.js
@@ -3,12 +3,24 @@ const popUpBg = document.querySelector(".popup-bg")
 const closePopUp = document.querySelector(".fa-times")
 const smallHeader = document.querySelector(".header-container")
 
+const intro = document.querySelector(".link-intro")
+const morad = document.querySelector(".link-morad")
+const schutz = document.querySelector(".link-schutz")
+const popupH1 = document.querySelector(".popup-h1")
+
+
 
 climberBtn.addEventListener("click", () => {
     popUpBg.classList.add("bg-active");
-    smallHeader.style.padding.add = "1em 3em;";
+    smallHeader.style.padding = "0.5em 3em";
 });
 
 closePopUp.addEventListener("click", () => {
     popUpBg.classList.remove("bg-active");
+    smallHeader.style.padding = "1.2em 3em";
 });
+
+intro.addEventListener("click", () => {
+    popupH1.innerHTML='Hello Malin'
+
+})
