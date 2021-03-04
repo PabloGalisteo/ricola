792e30063d827b0d53abc5d3307cae1afd3b5c35
Mieown <malinolofsson@hyperisland.se>
2021-03-04 15:43:40 +0100

    edit x icon, change font on tje popup and margins

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const climberBtn = document.querySelector(".climber-btn")
-const popUp = document.querySelector(".popup")
+const popUpBg = document.querySelector(".popup-bg")
+const closePopUp = document.querySelector(".fa-times")
+const smallHeader = document.querySelector(".header-container")
 
 
 climberBtn.addEventListener("click", () => {
-    hiddenPopup();
+    popUpBg.classList.add("bg-active");
+    smallHeader.style.padding.add = "1em 3em;";
 });
 
-function hiddenPopup() {
-    popUp.classList.remove("hidden");
-}
-
-
-
+closePopUp.addEventListener("click", () => {
+    popUpBg.classList.remove("bg-active");
+});
