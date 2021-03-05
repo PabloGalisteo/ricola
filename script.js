3cb9a2ae30f22e32c34300c5c081c65bcd4fbdf6
Pablo Galisteo <pablogalisteo86@gmail.com>
2021-03-05 16:15:53 +0100

    We add scroll in popUp and change image in different nature tab

--- script.js
+++ script.js
@@ -11,6 +11,8 @@ const transparency = document.querySelector(".link-transparency")
 
 const popupText = document.querySelector(".popup-text")
 const changeImg = document.querySelector(".popup-img")
+const linkIntro = document.querySelector(".link-intro")
+const linkNature = document.querySelector(".link-nature")
 
 
 climberBtn.addEventListener("click", () => {
@@ -26,7 +28,12 @@ closePopUp.addEventListener("click", () => {
 // Nature slide 
 
 intro.addEventListener("click", () => {
-    popUpBg.classList.remove("bg-active");
+    changeImg.src = "/images/footsteps.png";
+    popupText.innerHTML = "<h1 class='popup-h1'>"+'Our Footsteps Approach'+"</h1>"+
+                            "<h2 class='popup-h2'>"+'Sustainability strategy'+"</h2>"+
+                                "<p class='popup-paragraph'>"+'Understand how we can apply our sourcing standards from Switzerland in our global supply chain. We have high standards in our herb farming sourcing.<br> This footstep aims to explore how we can leverage and transfer our best practices (a combination of Swiss standards and Thomas Aeschlimann’s good work) to our global supply chain, in order to promote biodiversity and sustainable farming practices, while reducing potential reputational risks.'+"</p>"+
+                                "<h2 class='popup-h2'>"+'CURRENT STATE'+"</h2>"+
+                                "<p class='popup-paragraph'>"+'At Ricola we believe in the actions we take in the here and now. We believe that every step towards a more sustainable way of operating and developing our global business matters. The belief is grounded in Lean methodology, tangibility, behavioural and mathematical science. <br><br>If we take relevant and constant sustainable footsteps, over time, the accumulated effect will benefit us and the world in ways we could not imagine. <br><br>We refer to measures and projects as: “Footsteps”.'+"</p>";
 })
 
 nature.addEventListener("click", () => {
@@ -38,3 +45,5 @@ nature.addEventListener("click", () => {
                                 "<p class='popup-paragraph'>"+'We work close with farmers in Switzerland to ensure high biodiversity standards. A majority of supplier is certified according to “BioSuisse“ standards and farmers in Switzerland that produce for us are adhering to high sustainable farming standards. This is not necessarily the case in our global supply chain. Through Close the Gap we have identified areas that need improvement and we wish to conduct this improvement based on a set of Ricola Principles.'+"</p>";
 })
 
+ // linkNature.style.display.add("inline-block");
+// linkIntro.style.display.add("none");
