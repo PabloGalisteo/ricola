e60b927b44180c7ec4a7038a4a615066bc4384ef
Mieown <malinolofsson@hyperisland.se>
2021-03-05 12:18:13 +0100

    added function and manipulated DOM in JS so we can change info and image in the popup menu, and fixed scroll when text overlay

--- script.js
+++ script.js
@@ -9,10 +9,8 @@ const people = document.querySelector(".link-people")
 const business = document.querySelector(".link-business")
 const transparency = document.querySelector(".link-transparency")
 
-const popupH1 = document.querySelector(".popup-h1")
-const popupH2 = document.querySelector(".popup-h2")
-const popupParagraph = document.querySelector(".popup-paragraph")
-
+const popupText = document.querySelector(".popup-text")
+const changeImg = document.querySelector(".popup-img")
 
 
 climberBtn.addEventListener("click", () => {
@@ -32,10 +30,11 @@ intro.addEventListener("click", () => {
 })
 
 nature.addEventListener("click", () => {
-    popupH1.innerHTML = 'Beyond Zero<br>“Towards Positive Enviromental impact”';
-    popupH2.innerHTML = 'INTENTION';
-    popupParagraph.innerHTML = 
-    ' Understand how we can apply our sourcing standards from Switzerland in our global supply chain. We have high standards in our herb farming sourcing.<br> This footstep aims to explore how we can leverage and transfer our best practices (a combination of Swiss standards and Thomas Aeschlimann’s good work) to our global supply chain, in order to promote biodiversity and sustainable farming practices, while reducing potential reputational risks.<br><br> 
-    popupH2.innerHTML = 'CURRENT STATE'; <br> We work close with farmers in Switzerland to ensure high biodiversity standards. A majority of supplier is certified according to “BioSuisse“ standards and farmers in Switzerland that produce for us are adhering to high sustainable farming standards. This is not necessarily the case in our global supply chain. Through Close the Gap we have identified areas that need improvement and we wish to conduct this improvement based on a set of Ricola Principles.';
+    changeImg.src = "/images/footsteps_nature_croped.jpg";
+    popupText.innerHTML = "<h1 class='popup-h1'>"+'Beyond Zero Twards Positive Enviromental impact'+"</h1>"+
+                            "<h2 class='popup-h2'>"+'INTENTION'+"</h2>"+
+                                "<p class='popup-paragraph'>"+'Understand how we can apply our sourcing standards from Switzerland in our global supply chain. We have high standards in our herb farming sourcing.<br> This footstep aims to explore how we can leverage and transfer our best practices (a combination of Swiss standards and Thomas Aeschlimann’s good work) to our global supply chain, in order to promote biodiversity and sustainable farming practices, while reducing potential reputational risks.'+"</p>"+
+                                "<h2 class='popup-h2'>"+'CURRENT STATE'+"</h2>"+
+                                "<p class='popup-paragraph'>"+'We work close with farmers in Switzerland to ensure high biodiversity standards. A majority of supplier is certified according to “BioSuisse“ standards and farmers in Switzerland that produce for us are adhering to high sustainable farming standards. This is not necessarily the case in our global supply chain. Through Close the Gap we have identified areas that need improvement and we wish to conduct this improvement based on a set of Ricola Principles.'+"</p>";
 })
 
